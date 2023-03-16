816f3f3c39e80870a013b9e5187b3724fb741666
61-5a <azaslam121@gmail.com>
2023-03-16 16:48:39 +0530

    otp redirection , category page complete, view order popup fix

--- pages/category/[id].js
+++ pages/category/[id].js
@@ -1,16 +1,21 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+import { StoreContext } from "@/global/StoreContext";
+import backend from "@/global/backend";
+import Head from "next/head";
 import Header from "@/src/components/common/Header";
 import Footer from "@/src/components/common/Footer";
 import CategoryPageMain from "@/src/components/category";
-import Head from "next/head";
 
 import styles from "./index.module.css";
 
-export default function OrderListPage() {
+const OrderListPage = (props) => {
+  const name = props.data.subcategories[0].category_id.category_name;
   return (
     <>
       <Head>
-        <title>Order List | Arclif</title>
-        <meta name="description" content="Order list for your projects." />
+        <title>{name} | Arclif</title>
+        <meta name="description" content={`${name} for your projects.`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
@@ -25,4 +30,23 @@ export default function OrderListPage() {
       </main>
     </>
   );
+};
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const id = params.id;
+
+  // Fetch data from external API
+  const res = await fetch(`${backend}/admin/product/sub_category/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+  const data = await res.json();
+
+  return { props: { data } };
 }
+
+export default OrderListPage;
