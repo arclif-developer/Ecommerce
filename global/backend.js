2b9c0b090301d06b519165cfd32d37923aa617a2
shijin-benny <mr.shijinbenny361@gmail.com>
2023-03-21 19:51:59 +0530

    shijin

--- global/backend.js
+++ global/backend.js
@@ -1,3 +1,3 @@
-// var backend = "https://agrihav2-production.up.railway.app";
-var backend = "http://localhost:8080";
+var backend = "https://agrihav2-production.up.railway.app";
+// var backend = "http://localhost:8080";
 export default backend;
