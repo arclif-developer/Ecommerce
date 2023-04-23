83c871a1546e48a731e2c071cbbce6820d0e212c
shijin-benny <mr.shijinbenny361@gmail.com>
2023-04-23 11:58:42 +0530

    wallet com

--- src/components/cart/CartPageMain.js
+++ src/components/cart/CartPageMain.js
@@ -160,13 +160,13 @@ const CartPageMain = () => {
             <div className={styles.cartPage_left_products_container}>
               {cartItems?.map((items, index) => {
                 {
-                  order[index]?.productId === items?.product_id._id ? (
+                  order[index]?.productId === items?.product_id?._id ? (
                     ""
                   ) : (
                     <>
                       {items?.product_id?.seller_id
                         ? order.push({
-                            productId: items?.product_id._id,
+                            productId: items?.product_id?._id,
                             quantity: items?.quantity,
                             seller_id: items?.product_id?.seller_id,
                             amount:
@@ -176,7 +176,7 @@ const CartPageMain = () => {
                               ) * items?.quantity,
                           })
                         : order.push({
-                            productId: items?.product_id._id,
+                            productId: items?.product_id?._id,
                             quantity: items?.quantity,
                             admin: true,
                             amount:
